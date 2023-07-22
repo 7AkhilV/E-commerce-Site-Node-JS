73a5201ff1fc0f367cba5fb262ba151f43259560
Akhil V <7akhilv@gmail.com>
2023-07-22 16:03:03 +0530

    added connect-flash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,32 @@
 const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -23,7 +36,8 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        res.redirect("/login");
+        req.flash("error", "Invalid email or password.");
+        return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
@@ -36,6 +50,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
+          req.flash("error", "Invalid email or password.");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -53,19 +68,25 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
-        res.redirect("/signup");
+        req.flash(
+          "error",
+          "E-Mail exists already, please pick a different one."
+        );
+        return res.redirect("/signup");
       }
-      return bcrypt.hash(password, 12).then((hashedPassword) => {
-        const user = new User({
-          email: email,
-          password: hashedPassword,
-          cart: { items: [] },
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
         });
-        return user.save();
-      });
-    })
-    .then((result) => {
-      res.redirect("/login");
     })
     .catch((err) => {
       console.log(err);
--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    reuired: true,
+    required: true,
   },
   cart: {
     items: [

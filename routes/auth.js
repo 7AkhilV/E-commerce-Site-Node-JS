112238108491426f007b749c1df0512fb7ffb7ef
Akhil V <7akhilv@gmail.com>
2023-07-24 23:54:29 +0530

    added validation to user input

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -8,10 +10,64 @@ router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Invalid Email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (!user) {
+            return Promise.reject("Invalid email ");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").isLength({ min: 5 }).withMessage("Incorrect Password").trim(),
+  ],
+  authController.postLogin
+);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    // valdiating email and password for sign up
+    body("email")
+      .isEmail()
+      .withMessage("Enter a valid e-mail")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          //async validation
+          if (userDoc) {
+            return Promise.reject(
+              "E-Mail exists already, please pick a different one."
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters long").trim(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password has to match");
+      }
+      return true;
+    }).trim(),
+  ],
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
